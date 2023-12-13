8e6b40606fae804e106a9560546e33776a60a15c
Lubnnia <lubnnia@gmail.com>
2023-12-13 16:40:59 -0300

    feat: Create component and story TextArea, Checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { Check } from 'phosphor-react'
+
+import { CheckBoxContainer, CheckBoxIndicator } from './styles'
+
+export type CheckboxProps = ComponentProps<typeof CheckBoxContainer>
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckBoxContainer {...props}>
+      <CheckBoxIndicator asChild>
+        <Check weight="bold" />
+      </CheckBoxIndicator>
+    </CheckBoxContainer>
+  )
+}
